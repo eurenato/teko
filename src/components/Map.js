efe3c8778d36f5a8c8e6ecef28be57aa7b639298
RaissaOliv <raissa130704@gmail.com>
2024-05-21 15:27:16 -0300

    DASHBOARD FIX

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,33 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
+
 import axios from 'axios';
 
 function MyMap() {
   const [position, setPosition] = useState([-1.489572059746894, -48.455529902548946]);
-
-  useEffect(() => {
-    axios.get('http://13.59.94.236:8000/trashLocation/')
-      .then(response => {
-        const data = response.data[0];
-        console.log(data)
-        // Supondo que os dados tenham campos de latitude e longitude
-        setPosition([data.latitude, data.longitude]);
-      })
-      .catch(error => {
-        console.error("Erro ao buscar dados", error);
-      });
-  }, []);
-
   return (
     <MapContainer center={position} zoom={20} style={{ height: "300px", width: "500px" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          A posição retornada pela API
-        </Popup>
+      <Marker position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
       </Marker>
     </MapContainer>
   );
--- src/screens/Dashboard/DashBoard.js
+++ src/screens/Dashboard/DashBoard.js
@@ -4,24 +4,26 @@ import Map from '../../components/Map'
 import 'leaflet/dist/leaflet.css';
 import NavbarDash from '../../components/NavbarDash'
 
-
 function DashBoard() {
   return (
 
-    <><NavbarDash />
-      <div className="DashBoard">
-        <div className="outerChartContainer">
-            <div className="chartContainer">
+    <div className='main'>
+      <NavbarDash />
+        <div className="DashBoard">
+        <h1>Dados de coleta</h1>
+          <div className="outerChartContainer">
+              <div className="chartContainer">
               <ColumnChart />
-            </div>
-        </div>
+              </div>
+          </div>
+          <h1>Localização da lixeira</h1>
           <div className="outerMapContainer">
             <div className="mapContainer">
               <Map />
             </div>
         </div>
       </div>
-    </>
+  </div>
     
   );
 }
