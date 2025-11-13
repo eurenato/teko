d5289bf00c6d5b7859da67bbeb7ce8ca31232918
RaissaOliv <raissa130704@gmail.com>
2025-11-13 03:45:12 -0300

    o

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,15 +8,17 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-}
+    apiKey: "AIzaSyB-U1diP4O6fsezR6kYULjPMv7JWYfjmVY",
+  authDomain: "tekoagua-app.firebaseapp.com",
+  databaseURL: "https://tekoagua-app-default-rtdb.firebaseio.com",
+  projectId: "tekoagua-app",
+  storageBucket: "tekoagua-app.firebasestorage.app",
+  messagingSenderId: "268329163771",
+  appId: "1:268329163771:web:9309d646dd2d037589095e",
+  measurementId: "G-67W66L6BZ0",
+  databaseURL: "https://tekoagua-app-default-rtdb.firebaseio.com/"
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
